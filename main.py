49414d457913bf3210c7e4c59eb42694e403d1b7
Jeet Waral <jeetwaral@gmail.com>
2026-07-02 15:30:54 +0530

    Add database model and initialization for product management

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from models import Product
+# for Database
+from database import session, engine
+import database_model 
+
+
 
 app=FastAPI()
+
+
 
 @app.get("/")
 def home():
@@ -19,8 +26,32 @@ Products = [
     Product(id=10, name= "T-Shirt",description ="Mens's T-Shirts", price =999, quantity=20 )
 ]
 
+
+# database Connection
+database_model.Base.metadata.create_all(bind=engine)
+def init_db():
+
+    db = session()
+    count = db.query(database_model.Product).count() # Nothing but select count(*) from product(In Mysql)
+    
+    # So we craeted count varaible to check if there is data already in the Db.
+    # As we are running the script again and again it will try to add the same thing again and again.
+    if count ==0:
+        for product in Products:
+            #db.add(product) So ths will not work as it is a model of pydantci and not Alechemy.
+            db.add(database_model.Product(**product.model_dump()))
+            # We will need to pass the Db object, so we are basically converting the pydantic model to database model
+            # The database_model.product expects a key value pair to create a object for it.
+            # model_dum() gives a dictionary
+            # So we will not need dictionary we will need actuall key and values and thus we used **- unpacking
+
+        db.commit()
+init_db()#
+
+
 @app.get("/products")
 def get_all_products():
+    
     return Products
 
 @app.get("/product/{id}")
